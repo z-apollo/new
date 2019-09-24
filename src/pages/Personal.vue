5f981738c097af079771b0446e7ce37d1bb36503
z-apollo <54058143+z-apollo@users.noreply.github.com>
2019-09-24 16:58:40 +0800

    05-渲染用户数据

--- src/pages/Personal.vue
+++ src/pages/Personal.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <div class="profile">
-      <img src="http://pic2.zhimg.com/50/v2-f34145b01ab1d5bb463cac35ddc9777d_hd.jpg" alt />
+      <!-- <img src="http://pic2.zhimg.com/50/v2-f34145b01ab1d5bb463cac35ddc9777d_hd.jpg" alt /> -->
+      <!-- $axios.defaults.baseURL读取axios的服务器路径 -->
+      <img :src="$axios.defaults.baseURL + profile.head_img" alt="">
 
       <div class="profile-center">
         <div class="name">
           <span class="iconfont iconxingbienan"></span>
-          火星网友
+          <!-- 火星网友 -->
+          {{profile.nickname}}
         </div>
         <div class="time">2019.10.1</div>
       </div>
@@ -15,38 +18,48 @@
     </div>
 
     <!-- 调用条形组件 -->
-        <CellBar label="我的关注" text="关注的用户"/>
+    <CellBar label="我的关注" text="关注的用户" />
 
-        <CellBar label="我的跟帖" text="跟帖/回复"/>
+    <CellBar label="我的跟帖" text="跟帖/回复" />
 
-        <CellBar label="我的收藏" text="文章/视频"/>
+    <CellBar label="我的收藏" text="文章/视频" />
 
-        <CellBar label="设置"/>
+    <CellBar label="设置" />
   </div>
 </template>
 
 <script>
 // 导入条形组件
-import CellBar from "@/components/CellBar"
+import CellBar from "@/components/CellBar";
 
 export default {
-    components: {
-        CellBar
+    data(){
+        return{
+            //个人信息
+            profile:{}
+        }
     },
 
-    mounted(){
-        //请求个人资料接口
-        this.$axios({
-            url: "/user/" + localStorage.getItem("user_id"),
-            //添加头信息
-            headers:{
-                Authorization: localStorage.getItem("token")
-            }
-        }).then(res =>{
-            console.log(res)
-        })
-    }
-}
+  components: {
+    CellBar
+  },
+
+  mounted() {
+    //请求个人资料接口
+    this.$axios({
+      url: "/user/" + localStorage.getItem("user_id"),
+      //添加头信息
+      headers: {
+        Authorization: localStorage.getItem("token")
+      }
+    }).then(res => {
+        console.log(res);
+        const{data} = res.data;
+        //保存到data
+        this.profile = data;
+    });
+  }
+};
 </script>
 
 <style scoped lang="less">
