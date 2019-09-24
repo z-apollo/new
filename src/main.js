d2da98ce98b278466fea5c8b60d93b9ef7e4589b
z-apollo <54058143+z-apollo@users.noreply.github.com>
2019-09-24 19:14:29 +0800

    07-路由守卫

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import Vue from "vue";
 
 // 路由：1.导入路由构造函数
@@ -9,7 +8,9 @@ import Vant from 'vant';
 import axios from "axios";
 
 // 导入组件
-import {Toast} from "vant";
+import {
+    Toast
+} from "vant";
 import App from "@/App";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
@@ -27,19 +28,47 @@ axios.defaults.baseURL = "http://localhost:3000";
 
 // 路由：2.创建路由配置
 const routes = [
-    { path: "/login", component: Login},
-    { path: "/register", component: Register},
-    { path: "/personal", component: Personal}
+    {path: "/login", component: Login},
+    {path: "/register", component: Register},
+    {path: "/personal", component: Personal}
 ]
 
 // 路由：3.创建对象
 const router = new VueRouter({
     routes,
 });
+
+//路由守卫：就是一页面跳转之前的拦截器
+//to：要跳转之后的页面，去哪里
+//from：跳转之前的页面，来自哪里
+//next：必须要调用next()。调用才会执行跳转，还可以重定向，next("/login")
+router.beforeEach((to, from, next) => {
+    // 是否有token
+    const hasToken = localStorage.getItem("token");
+
+    // 判断是否是需要登陆权限的页面
+    if (to.path === "/personal") {
+
+        // 判断本地是否有token
+        if (hasToken) {
+            // 正常跳转
+            next();
+        } else {
+            // 没有token正常跳转到登录
+            next("/login")
+        }
+
+    } else {
+        // 所有人都可以访问的页面正常浏览
+        next();
+    }
+
+})
+
 //响应拦截器 拦截响应信息
-axios.interceptors.response.use(res=>{
-    const{message,statusCode} =res.data
-    if(statusCode ===401){
+axios.interceptors.response.use(res => {
+    const {message, statusCode} = res.data
+    if (statusCode === 401) {
         Toast.fail(message)
     }
     return res
@@ -52,9 +81,8 @@ new Vue({
     router,
 
     // 指定一个组件渲染根实例，这个组件可以成为最底层的组件
-    render(createElement){
+    render(createElement) {
         // render函数使用固定的写法，只有App是可变；
         return createElement(App);
     }
 })
-
