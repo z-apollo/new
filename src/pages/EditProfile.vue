feffce20b55414fcad690f635837fd4f3aa4b286
z-apollo <54058143+z-apollo@users.noreply.github.com>
2019-09-25 16:58:48 +0800

    13-渲染编辑资料页的数据

--- src/pages/EditProfile.vue
+++ src/pages/EditProfile.vue
@@ -5,13 +5,13 @@
 
       <!-- 头像 -->
       <div class="head">
-          <img src="../../static/man.png" alt/>
+          <img :src="profile.head_img" alt/>
       </div>
 
       <!-- 调用条形组件 -->
-      <CellBar label="昵称" text="火星网友" />
-      <CellBar label="密码" text="123" />
-      <CellBar label="性别" text="男" />
+      <CellBar label="昵称" :text="profile.nickname" />
+      <CellBar label="密码" :text="profile.password" type="password" />
+      <CellBar label="性别" :text="profile.gender === 1? '男' : '女'" />
   </div>
 </template>
 
@@ -21,9 +21,40 @@ import HeaderNormal from "@/components/HeaderNormal"
 //导入条形
 import CellBar from "@/components/CellBar";
 export default {
+    data(){
+        return{
+            //用户详情
+            profile:{}
+        }
+    },
+
     components:{
         HeaderNormal,
         CellBar
+    },
+
+    mounted(){
+        //请求个人资料接口
+        this.$axios({
+            url:"/user/" + localStorage.getItem("user_id"),
+            //添加头信息
+            headers: {
+                Authorization: localStorage.getItem("token")
+            }
+        }).then(res =>{
+            const{ data } = res.data;
+            
+            if(data){
+                //保存到data
+                this.profile = data;
+                //如果用户有头像
+                if(data.head_img){
+                    this.profile.head_img = this.$axios.defaults.baseURL + data.head_img;
+                }else{
+                    this.profile.head_img = "../static/man.png";
+                }
+            }
+        });
     }
 }
 </script>
