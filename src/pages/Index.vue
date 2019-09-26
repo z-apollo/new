ccc67c3fa077ae6512ca5d783c4935cf61229f70
z-apollo <54058143+z-apollo@users.noreply.github.com>
2019-09-26 20:08:02 +0800

    32-渲染栏目列表

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -21,9 +21,9 @@
     <!-- title：要显示的内容 -->
     <van-tabs v-model="active" sticky swipeable>
       <van-tab 
-      v-for="index in 8" 
+      v-for="(item, index) in categories" 
       :key="index"
-      :title="'标签 ' + index"
+      :title="item.name"
       >
 
         <p v-for="index in 10">
@@ -43,12 +43,34 @@ import PostCard from "@/components/PostCard"
 export default {
     data(){
         return{
-            active: 1
+            //当前默认的栏目，没有登录应该等于0，有登录等于1，最终的效果为了默认显示头条
+            active: localStorage.getItem("token")? 1 : 0,
+            //栏目列表
+            categories: []
         }
     },
     //注册组件
     components:{
         PostCard
+    },
+
+    mounted(){
+        const config ={
+            url:"/category",
+        }
+        //是否存在token，如果有就给头部加上token验证
+        if(localStorage.getItem("token")){
+            config.headers = {
+                 Authorization: localStorage.getItem("token")
+            }
+        }
+        //请求栏目的数据
+        this.$axios(config)
+        .then(res=>{
+            const {data} = res.data;
+            //保存栏目列表
+            this.categories = data;
+        })
     }
 };
 </script>
